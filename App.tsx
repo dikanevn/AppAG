60e972d4ddc74ff06e7d59120c136fbea80ca8aa
dikanevn <dikanevn@gmail.com>
2025-06-21 20:59:18 +0300

    q

--- App.tsx
+++ App.tsx
@@ -12,7 +12,7 @@ export default function App() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'lightblue' }}>
       <Text style={{ fontSize: 24, color: 'darkblue' }}>
-        5 Привет, Web!
+        6 Привет, Web!
       </Text>
     </View>
   );
